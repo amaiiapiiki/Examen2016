1417a73e37bf644bfe5aad952052fad676d9f733
ssc1999 <ssc1099.csmm@gmail.com>
2020-05-01 18:40:28 +0200

    Familia classes

---
+++ Familia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+#include "Familia.h"
+#include "Miembro.h"
+using namespace std;
+
+
+
+
---
+++ Familia.h
@@ -0,0 +1,14 @@
+#ifndef FAMILIA_H_
+#define FAMILIA_H_
+
+class Familia{
+private:
+	Miembro marido;
+    Miembro mujer;
+public:
+
+
+	
+};
+
+#endif /* FAMILIA_H_ */
