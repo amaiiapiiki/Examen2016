130048264bc9c81846c3ed8f6e46cf2a60b00530
ssc1999 <ssc1099.csmm@gmail.com>
2020-05-01 19:00:16 +0200

    trying familia

--- Familia.cpp
+++ Familia.cpp
@@ -6,6 +6,49 @@
 #include "Miembro.h"
 using namespace std;
 
+Familia::Familia(const char* nombre, int edad)
+{
+	this.super();
+}
+Familia::Familia(const Familia& p)
+{
+	this->nombre = new char[strlen(p.nombre) + 1];
+	strcpy(this->nombre, p.nombre);
+	this->edad = p.edad;
 
+}
+Familia::~Familia()
+{
+	delete[] this->nombre;
+}
+char* Familia::getNombre() const
+{
+	return this->nombre;
+}
+int Familia::getEdad() const
+{
+	return this->edad;
+}
+void Familia::setNombre(const char* nombre)
+{
+	delete[] this->nombre;
+	this->nombre = new char[strlen(nombre) + 1];
+	strcpy(this->nombre, nombre);
+}
+void Familia::setEdad(int edad)
+{
+	this->edad = edad;
+}
+char* Familia::toStr() const
+{
+	char* str = new char[100];
+	char edad[5];
+	itoa (this->edad, edad, 10);
+	strcpy(str, this->nombre);
+	strcat(str, " (");
+	strcat(str, edad);
+	strcat(str, " a�os)");
+	return str;
+}
 
 
--- Familia.h
+++ Familia.h
@@ -6,7 +6,15 @@ private:
 	Miembro marido;
     Miembro mujer;
 public:
-
+    Familia();
+	Familia(const char*, int);
+	Familia(const Familia&);
+	~Familia();
+	char* getNombre() const;
+	void setNombre(const char*);
+	int getEdad() const;
+	void setEdad(int);
+	char* toStr() const;
 
 	
 };
